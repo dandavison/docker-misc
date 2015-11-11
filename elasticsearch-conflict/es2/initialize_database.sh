9a13a159bbd058582ba9c87ad084b00b2aae825c
Dan Davison <dan@counsyl.com>
2015-11-10 21:51:32 -0800

    Revert "Don't create index in es2"
    This reverts commit 7efd3facbbc0594ebc123caee9690d9407ea553f.

--- elasticsearch-conflict/es2/initialize_database.sh
+++ elasticsearch-conflict/es2/initialize_database.sh
@@ -1,6 +1,8 @@
 # Start elasticsearch on localhost to prevent outside services from connecting until initialization is done
 elasticsearch -d -Des.network.host=localhost
 
+until bash create_index.sh > /dev/null 2>&1 ; do sleep 1 ; done
+
 kill $(ps aux | grep java | grep -v grep | awk '{print $2}')
 
 # Restart elasticsearch on 0.0.0.0 to allow external connections
