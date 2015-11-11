005984d4f98cad66d7f846a183a9dcb1425bb5f5
Dan Davison <dan@counsyl.com>
2015-11-10 23:32:18 -0800

    Only start es1
    - Initially, es1 has only its own index
    
    - This remains true when indexing is run manually on es2 against es2
      localhost
    
    - But when es2 server starts on 0.0.0.0, es1 aquires es2's index
    
    At that point, es2 logs this:
    
    root@2ed76b2d3190:/elasticsearch# elasticsearch -Des.network.host=0.0.0.0
    [2015-11-11 08:00:37,793][INFO ][node                     ] [Wanda Maximoff] version[1.7.3], pid[89], build[05d4530/2015-10-15T09:14:17Z]
    [2015-11-11 08:00:37,795][INFO ][node                     ] [Wanda Maximoff] initializing ...
    [2015-11-11 08:00:37,898][INFO ][plugins                  ] [Wanda Maximoff] loaded [], sites []
    [2015-11-11 08:00:37,931][INFO ][env                      ] [Wanda Maximoff] using [1] data paths, mounts [[/ (none)]], net usable_space [10.4gb], net total_space [18.1gb], types [aufs]
    [2015-11-11 08:00:40,275][INFO ][node                     ] [Wanda Maximoff] initialized
    [2015-11-11 08:00:40,276][INFO ][node                     ] [Wanda Maximoff] starting ...
    [2015-11-11 08:00:40,346][INFO ][transport                ] [Wanda Maximoff] bound_address {inet[/0:0:0:0:0:0:0:0:9300]}, publish_address {inet[/172.17.0.2:9300]}
    [2015-11-11 08:00:40,369][INFO ][discovery                ] [Wanda Maximoff] elasticsearch/gH44N0qqSUS6OJ4sG3GXfw
    [2015-11-11 08:00:43,461][INFO ][cluster.service          ] [Wanda Maximoff] detected_master [Cerise][I-YcaL0lQUSxkJQuYlBneQ][768de1dc8185][inet[/172.17.0.3:9300]], added {[Cerise][I-YcaL0lQUSxkJQuYlBneQ][768de1dc8185][inet[/172.17.0.3:9300]],}, reason: zen-disco-receive(from master [[Cerise][I-YcaL0lQUSxkJQuYlBneQ][768de1dc8185][inet[/172.17.0.3:9300]]])
    [2015-11-11 08:00:43,479][INFO ][gateway.local.state.meta ] [Wanda Maximoff] [test_index_es2] dangling index, exists on local file system, but not in cluster metadata, auto import to cluster state [YES]
    [2015-11-11 08:00:43,520][INFO ][http                     ] [Wanda Maximoff] bound_address {inet[/0:0:0:0:0:0:0:0:9200]}, publish_address {inet[/172.17.0.2:9200]}
    [2015-11-11 08:00:43,520][INFO ][node                     ] [Wanda
    Maximoff] started
    
    and es1 logs this:
    
    es1 | [2015-11-11 08:00:43,445][INFO ][cluster.service          ] [Cerise] added {[Wanda Maximoff][gH44N0qqSUS6OJ4sG3GXfw][2ed76b2d3190][inet[/172.17.0.2:9300]],}, reason: zen-disco-receive(join from node[[Wanda Maximoff][gH44N0qqSUS6OJ4sG3GXfw][2ed76b2d3190][inet[/172.17.0.2:9300]]])
    es1 | [2015-11-11 08:00:43,508][INFO ][gateway.local.state.meta ] [Cerise] auto importing dangled indices [test_index_es2/OPEN] from [[Wanda Maximoff][gH44N0qqSUS6OJ4sG3GXfw][2ed76b2d3190][inet[/172.17.0.2:9300]]]
    
    Then on shutdown of es2 this:
    
    es1 | [2015-11-11 08:05:16,439][INFO ][cluster.service          ] [Cerise] removed {[Wanda Maximoff][gH44N0qqSUS6OJ4sG3GXfw][2ed76b2d3190][inet[/172.17.0.2:9300]],}, reason: zen-disco-node_left([Wanda Maximoff][gH44N0qqSUS6OJ4sG3GXfw][2ed76b2d3190][inet[/172.17.0.2:9300]])
    es1 | [2015-11-11 08:05:16,441][INFO ][cluster.routing          ]
    [Cerise] delaying allocation for [10] unassigned shards, next check in
    [59.9s]
    
    es1 /etc/hosts:
    172.17.0.3      768de1dc8185
    127.0.0.1       localhost
    ::1     localhost ip6-localhost ip6-loopback
    fe00::0 ip6-localnet
    ff00::0 ip6-mcastprefix
    ff02::1 ip6-allnodes
    ff02::2 ip6-allrouters
    
    es2 /etc/hosts:
    172.17.0.2      2ed76b2d3190
    127.0.0.1       localhost
    ::1     localhost ip6-localhost ip6-loopback
    fe00::0 ip6-localnet
    ff00::0 ip6-mcastprefix
    ff02::1 ip6-allnodes
    ff02::2 ip6-allrouters

--- elasticsearch-conflict/es1/initialize_database.sh
+++ elasticsearch-conflict/es1/initialize_database.sh
@@ -6,4 +6,6 @@ until bash create_index.sh > /dev/null 2>&1 ; do sleep 1 ; done
 kill $(ps aux | grep java | grep -v grep | awk '{print $2}')
 
 # Restart elasticsearch on 0.0.0.0 to allow external connections
-exec elasticsearch -Des.network.host=localhost
+elasticsearch -Des.network.host=0.0.0.0 &
+
+exec sleep infinity
