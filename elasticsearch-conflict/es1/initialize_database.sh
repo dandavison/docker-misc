f479f23bdbc2b1eade2b9257279c4a9046c047be
Dan Davison <dan@counsyl.com>
2015-11-10 22:02:03 -0800

    Don't restart ES1
    `Connection refused` when making the requests in ES1 container

--- elasticsearch-conflict/es1/initialize_database.sh
+++ elasticsearch-conflict/es1/initialize_database.sh
@@ -5,5 +5,4 @@ until bash create_index.sh > /dev/null 2>&1 ; do sleep 1 ; done
 
 kill $(ps aux | grep java | grep -v grep | awk '{print $2}')
 
-# Restart elasticsearch on 0.0.0.0 to allow external connections
-exec elasticsearch -Des.network.host=localhost
+exec sleep infinity
